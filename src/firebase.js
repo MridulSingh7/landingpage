cda1767ad7cbcbda88d9ae25e4a882f965cf36fb
Kumar Mridul <mridulsingh425.personal@gmail.com>
2025-07-29 12:53:39 +0530

    setting up

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const initializeApp = require('firebase/app');
+const getAnalystics = require('firebase/analytics');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsu68suRo3tCErrJzBs_Q3nBm2WNHP9xI",
+  authDomain: "landingpage-20500.firebaseapp.com",
+  projectId: "landingpage-20500",
+  storageBucket: "landingpage-20500.firebasestorage.app",
+  messagingSenderId: "902737508635",
+  appId: "1:902737508635:web:e4c2908f62513b9900fbbe",
+  measurementId: "G-19490Q93QX"
+};
